a16574d8bd6ff3152b195c01c318c27d9a19afc0
1gprotein <132429340+1gprotein@users.noreply.github.com>
2023-05-03 22:50:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function displayText() {
+    document.getElementById("secret").innerHTML = "♡｡ﾟ.(*♡´‿` 人´‿` ♡*)ﾟ♡ °・ <br> Dear shivain(baby),<br> Happy birthday! YOU LIVED ON MORE YEAR HURRAY!!! and im glad i was in it again >< I hope i'm able to celebrate alll your birthday in the future with you.<p>&hearts;<p>";
+}
+
+function removeText() {
+    document.getElementById("secret").innerHTML = "";
+}
