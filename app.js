970726d774243bfaeea59a3298fd03f16ed3b82c
1gprotein <132429340+1gprotein@users.noreply.github.com>
2023-05-04 19:59:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 function displayText() {
-    document.getElementById("secret").innerHTML = "♡｡ﾟ.(*♡´‿` 人´‿` ♡*)ﾟ♡ °・ <br> Dear shivain(baby),<br> Happy birthday! YOU LIVED ON MORE YEAR HURRAY!!! and im glad i was in it again >< I hope i'm able to celebrate alll your birthday in the future with you.<p>&hearts;<p>";
+    document.getElementById("secret").innerHTML = "♡｡ﾟ.(*♡´‿` 人´‿` ♡*)ﾟ♡ °・ <br> To my darling, <br>
+happy birthday my beautiful boy (⁠灬⁠º⁠‿⁠º⁠灬⁠)⁠♡<br>
+I love you and i hope you have an amazing day today. Im so glad that i am able to call you boyfriend hehe. You really came into my life and showed me what true love is (twice). If you were not born i would not be able to experience true love with my soulmate so I'm veerryyyy grateful for this day ><. 
+<br>Xoxo, <br>
+Your girl <p>&hearts;<p>";
 }
 
 function removeText() {
