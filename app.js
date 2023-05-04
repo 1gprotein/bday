3dde602d99580c6d69ef4fefa2e43759c15676eb
1gprotein <132429340+1gprotein@users.noreply.github.com>
2023-05-04 20:04:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function displayText() {
     document.getElementById("secret").innerHTML = "♡｡ﾟ.(*♡´‿` 人´‿` ♡*)ﾟ♡ °・ <br> To my darling, <br>
-happy birthday my beautiful boy (⁠灬⁠º⁠‿⁠º⁠灬⁠)⁠♡<br>
-I love you and i hope you have an amazing day today. Im so glad that i am able to call you boyfriend hehe. You really came into my life and showed me what true love is (twice). If you were not born i would not be able to experience true love with my soulmate so I'm veerryyyy grateful for this day ><. 
+happy birthday my beautiful boy (⁠灬⁠º⁠‿⁠º⁠灬⁠)⁠♡ <br>
+I love you and i hope you have an amazing day today. Im so glad that i am able to call you boyfriend hehe. You really came into my life and showed me what true love is (twice). If you were not born i would not be able to experience true love with my soulmate so I'm veerryyyy grateful to your parents for this day ><. 
 <br>Xoxo, <br>
 Your girl <p>&hearts;<p>";
 }
